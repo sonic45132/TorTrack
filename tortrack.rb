fe5e9e25a0a879398f9efc3f03c2f16164d5ceaf
Chris Brice <sonicblue700@gmail.com>
2016-01-13 23:00:12 +0000

    added local jquery and started basic layout

--- tortrack.rb
+++ tortrack.rb
@@ -3,6 +3,10 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port, 8080
 
+before do
+  cache_control :public, :must_revalidate, :max_age => 60
+end
+
 get '/' do
-  "Hello World!"
+  erb :torrents
 end
